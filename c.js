aa6853b3c0a27a9863810847180028866df94099
Ayush Pathak <62694274+ayshptk@users.noreply.github.com>
2021-04-11 18:51:02 +0530

    Calling Cash.min.js directly

--- c.js
+++ c.js
@@ -1,2 +1,24 @@
-$(".c").on("click", function() {$(".cw").toggleClass("copen");});
-        
+
+function include(filename, onload) {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.src = filename;
+        script.type = 'text/javascript';
+        script.onload = script.onreadystatechange = function() {
+            if (script.readyState) {
+                if (script.readyState === 'complete' || script.readyState === 'loaded') {
+                    script.onreadystatechange = null;                                                  
+                    onload();
+                }
+            } 
+            else {
+                onload();          
+            }
+        };
+        head.appendChild(script);
+    }
+    
+    include('https://cdnjs.cloudflare.com/ajax/libs/cash/8.1.0/cash.min.js', function() {
+        $(".c").on("click", function() {$(".cw").toggleClass("copen");});
+
+    });
